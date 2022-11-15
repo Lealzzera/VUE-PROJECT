cabeb0dcd7c84721470873e591bc732df781e309
vinicius <vinicius@beaver.com.br>
2022-11-15 12:43:37 -0300

    Change home

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -5,162 +5,89 @@
 		<main class="view">
 			<div class="container">
 				<div class="row">
-					<div class="col-6">
+					<div class="col-7">
 						<div class="limiter">
 							<h2>Limitadores em uso</h2>
-
-							<div class="list-distribution">
-								<div class="proccess-item">
-									<span class="proccess-quantity">50 processos</span>
-									<span class="category">Cível</span>
+							<div class="list">
+								<div class="row itens">
+									<div class="col-6 process">50 processos</div>
+									<div class="col-6 category">Família</div>
 								</div>
 
-								<div class="proccess-item">
-									<span class="proccess-quantity">30% da média mensal</span>
-									<span class="category">Família</span>
+								<div class="row itens">
+									<div class="col-6 process">50 processos</div>
+									<div class="col-6 category">Cível</div>
 								</div>
 
-								<div class="proccess-item">
-									<span class="proccess-quantity">50 processos</span>
-									<span class="category">Cível</span>
+								<div class="row itens">
+									<div class="col-6 process">50 processos</div>
+									<div class="col-6 category">Família</div>
 								</div>
-					
-								<div class="proccess-item">
-									<span class="proccess-quantity">50 processos</span>
-									<span class="category">Cível</span>
-								</div>
-						
-								<div class="proccess-item">
-									<span class="proccess-quantity">50 processos</span>
-									<span class="category">Cível</span>
-								</div>
-						
-								<div class="proccess-item">
-									<span class="proccess-quantity">25% da média mensal</span>
-									<span class="category">Criminal</span>
+
+								<div class="row itens">
+									<div class="col-6 process">50 processos</div>
+									<div class="col-6 category">Cível</div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-6"></div>
 				</div>
 
 				<div class="row">
-					<div class="col-6">
-						<div class="distribution"></div>
+					<div class="col-7">
+						<div class="limiter">
+							<h2>Distribuições recentes</h2>
+							<div class="row itens">
+								<div class="col-4 title">Processos</div>
+								<div class="col-4 title">Classificação</div>
+							</div>
+							<div class="list">
+								<div class="row itens">
+									<div class="col-4 process">50 processos</div>
+									<div class="col-4 category">Cível</div>
+									<div class="col-3"></div>
+									<div class="col-1"></div>
+								</div>
+
+								<div class="row itens">
+									<div class="col-4 process">50 processos</div>
+									<div class="col-4 category">Cível</div>
+									<div class="col-3"></div>
+									<div class="col-1"></div>
+								</div>
+
+								<div class="row itens">
+									<div class="col-4 process">50 processos</div>
+									<div class="col-4 category">Cível</div>
+									<div class="col-3"></div>
+									<div class="col-1"></div>
+								</div>
+
+								<div class="row itens">
+									<div class="col-4 process">50 processos</div>
+									<div class="col-4 category">Cível</div>
+									<div class="col-3"></div>
+									<div class="col-1"></div>
+								</div>
+
+								<div class="row itens">
+									<div class="col-4 process">50 processos</div>
+									<div class="col-4 category">Cível</div>
+									<div class="col-3"></div>
+									<div class="col-1"></div>
+								</div>
+
+								<div class="row itens">
+									<div class="col-4 process">50 processos</div>
+									<div class="col-4 category">Cível</div>
+									<div class="col-3"></div>
+									<div class="col-1"></div>
+								</div>
+
+							</div>
+						</div>
 					</div>
-					<div class="col-6"></div>
 				</div>
-			</div>
-
-
-
-
-			
-			<h2>Limitadores em uso</h2>
-	
-			
-
-
-
-
-			<v-divider></v-divider>
-			<h2 class="distribuition">Distribuições recentes</h2>
-			<v-divider></v-divider>
-			<v-divider></v-divider>
-			<div class="distribuition-content">
-				<h3>Processos</h3>
-				<h3 class="classification">Classificação</h3>
-			</div>
-			<v-divider></v-divider>
-			<v-divider></v-divider>
-			<div>
-				<ul class="list-distribution">
-					<li class="distribution-list">
-						<span>6 processos</span>
-						<span class="classification-item"
-							>Classificação 01</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>54 processos</span>
-						<span class="classification-item"
-							>Classificação 02</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>36 processos</span>
-						<span class="classification-item"
-							>Classificação 03</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>16 processos</span>
-						<span class="classification-item"
-							>Classificação 04</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>6 processos</span>
-						<span class="classification-item"
-							>Classificação 01</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>54 processos</span>
-						<span class="classification-item"
-							>Classificação 02</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>36 processos</span>
-						<span class="classification-item"
-							>Classificação 03</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>16 processos</span>
-						<span class="classification-item"
-							>Classificação 04</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>6 processos</span>
-						<span class="classification-item"
-							>Classificação 01</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>54 processos</span>
-						<span class="classification-item"
-							>Classificação 02</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>36 processos</span>
-						<span class="classification-item"
-							>Classificação 03</span
-						>
-					</li>
-					<v-divider></v-divider>
-					<li class="distribution-list">
-						<span>16 processos</span>
-						<span class="classification-item"
-							>Classificação 04</span
-						>
-					</li>
-					<v-divider></v-divider>
-				</ul>
 			</div>
 		</main>
 	</section>
@@ -177,66 +104,38 @@
 	};
 </script>
 
-<style scoped>
+<style scoped lang="less">
 	main {
 		padding-top: 120px;
 		padding-left: 32px;
 		font-family: Segoe UI, Tahoma, Geneva, Verdana, sans-serif;
 		font-weight: 400;
 		max-height: 100vh;
-	}
-
-	.distribuition {
-		margin-top: 40px;
-	}
-
-	h2 {
-		font-family: Segoe UI, Tahoma, Geneva, Verdana, sans-serif;
-		font-weight: 400;
-	}
-
-	.proccess-item {
-		display: flex;
-		align-items: flex-start;
-		justify-content: space-between;
-		width: 500px;
-		margin: 25px 0px;
-		margin-right: 70px;
-		font-weight: bold;
-	}
-
-	.distribuition-content {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		margin: 15px 0px;
-	}
-
-	h3 {
-		font-weight: 400;
-		font-size: 16px;
-	}
-
-	.classification {
-		margin-right: 170px;
-	}
-
-	.classification-item {
-		margin-right: 140px;
-	}
-
-	.distribution-list {
-		display: flex;
-		justify-content: space-between;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		margin: 25px 0px;
-		font-weight: bold;
-	}
-
-	.list-distribution {
-		overflow-y: auto;
-		max-height: 30vh;
+		.limiter{
+			margin: 0 0 20px;
+			.itens{
+				.title{
+					font-weight: 600;
+					margin: 0 0 5px;
+				}
+			}
+			h2 {
+				font-family: Segoe UI, Tahoma, Geneva, Verdana, sans-serif;
+				font-size: 24px;
+				font-weight: 600;
+				margin: 0 0 11px;
+			}
+			.list{
+				overflow-y: auto;
+				max-height: 30vh;
+				.itens{
+					width: 100%;
+					position: relative;
+					margin: 0;
+					border-bottom: 1px solid #cecece;
+					padding: 4px 0;
+				}
+			}
+		}
 	}
 </style>
