3755614a324353f62f69fb0c7010cd6f0f96686c
vinicius <vinicius@beaver.com.br>
2022-11-15 15:26:45 -0300

    create distribute

--- src/views/DistributeView/DistributeView.vue
+++ src/views/DistributeView/DistributeView.vue
@@ -9,16 +9,27 @@
 						<h2>Distribuição de Processos</h2>
                         <form>
                             <div class="form-group">
-                                <p class="title-cut">Selecione a área</p>
-                                <input type="checkbox" name="civel" id="civel">
-                                <label for="civel">Cível</label>
-                                <input type="checkbox" name="criminal" id="criminal">
-                                <label for="criminal">Criminal</label>
-                                <input type="checkbox" name="familia" id="familia">
-                                <label for="familia">Familia</label>
+                                <p class="title">Selecione a área</p>
+                                <div class="check_group">
+                                    <input type="checkbox" name="civel" id="civel">
+                                    <i class="fa-solid fa-square-check"></i>
+                                    <label for="civel">Cível</label>
+                                </div>
+
+                                <div class="check_group">
+                                    <input type="checkbox" name="criminal" id="criminal">
+                                    <i class="fa-solid fa-square-check"></i>
+                                    <label for="criminal">Criminal</label>
+                                </div>
+
+                                <div class="check_group">
+                                    <input type="checkbox" name="familia" id="familia">
+                                    <i class="fa-solid fa-square-check"></i>
+                                    <label for="familia">Familia</label>
+                                </div>
                             </div>
                             <div class="form-group">
-                                <p class="title-area">Limitar orgão julgador</p>
+                                <p class="title">Limitar orgão julgador</p>
                                 <select name="distribuicao" id="distribution">
                                     <option value="valor1">Valor 1</option>
                                     <option value="valor2" selected>Valor 2</option>
@@ -32,17 +43,17 @@
 					<div class="col-6">
 						<h2>Distribuição</h2>
 						<div class="row itens">
-							<div class="col-4 title">Nome</div>
-							<div class="col-4 title">Vara/Grupo</div>
+							<div class="col-4 title">Processos</div>
+							<div class="col-4 title">Classificação</div>
 						</div>
 
 						<div class="list">
 							<div class="row itens" v-for="(processo, index) in processos" :key="index">
-								<div class="col-4">{{ processo.quantidade }}</div>
-								<div class="col-4">{{ processo.categoria }}</div>
+								<div class="col-4 process">{{ processo.quantidade }}</div>
+								<div class="col-4 classification">{{ processo.categoria }}</div>
 								<div class="col-2"></div>
-								<div class="col-1">del</div>
-								<div class="col-1">edit</div>
+								<div class="col-1 delete">del</div>
+								<div class="col-1 edit">edit</div>
 							</div>
 						</div>
 
@@ -59,7 +70,25 @@ import HeaderComponent from '@/components/HeaderComponent/HeaderComponent.vue'
         name: 'DistributeView',
         components: {
             HeaderComponent
-        }
+        },
+        data() {
+			return {
+				processos: [
+					{
+						quantidade: "Limitador 01",
+						categoria: "Cível",
+					},
+					{
+						quantidade: "Limitador 02",
+						categoria: "Família",
+					},
+					{
+						quantidade: "Limitador 03",
+						categoria: "Criminal",
+					},
+				],
+			};
+		}
     }
 </script>
 
@@ -95,17 +124,55 @@ import HeaderComponent from '@/components/HeaderComponent/HeaderComponent.vue'
 		}
         .form-group{
             margin: 0 0 20px;
+            .check_group{
+                display: inline-block;
+                position: relative;
+                i{
+                    display: none;
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    font-size: 20px;
+                    color: #8bc97b;
+                    z-index: 9;
+                }
+                input[type="checkbox"]{
+                    width: 18px;
+                    height: 18px;
+                    z-index: 999;
+                    cursor: pointer;
+                    &:checked{
+                        opacity: 0;
+                        z-index: 999;
+                        ~i{
+                            display: block;
+                        }
+                    }
+                }
+            }
             select{
-                widows: 100%;
+                width: 100%;
+                border-radius: 3px;
+                padding: 6px;
+                border: 1px solid #a5a5a5;
+            }
+            label{
+                margin: 0 20px 0 5px;
+                line-height: 14px;
+                top: -2px;
+                position: relative;
+            }
+            .title{
+                margin: 0 0 10px;
+                &:before{
+                    content: "*";
+                    color: red;
+                }
             }
         }
 	}
 
-	.title-area:before,
-	.title-cut:before {
-		content: "*";
-		color: red;
-	}
+	
 
 	.distribution-inputs input {
 		padding: 0px 0px 0px 8px;
